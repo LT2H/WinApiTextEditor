5d4f89d8fa13fbf71669d8aadd0cf05f2d25deff
LT2H <letranhuuhung@gmail.com>
2025-02-07 10:04:17 +0700

    feat: add basic controls

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -3,58 +3,104 @@
 
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 
+void AddControls(HWND hwnd);
+
+constexpr int OPEN_FILE_BUTTON{ 1 };
+constexpr int SAVE_FILE_BUTTON{ 2 };
+
 HWND hMainWindow{};
+HWND hEdit{};
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
-	// MessageBox(nullptr, L"HELLO", L"My title", MB_OK);
-	WNDCLASSW wc{};
+    // MessageBox(nullptr, L"HELLO", L"My title", MB_OK);
+    WNDCLASSW wc{};
 
-	wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW);
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wc.hInstance = hInst;
-	wc.lpszClassName = L"myWindowClass";
-	wc.lpfnWndProc = WindowProc;
+    wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW);
+    wc.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wc.hInstance     = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc   = WindowProc;
 
-	if (!RegisterClassW(&wc))
-		return -1;
+    if (!RegisterClass(&wc))
+        return -1;
 
-	hMainWindow = CreateWindowW(wc.lpszClassName,
-		L"My Title",
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100,
-		100,
-		800,
-		900,
-		nullptr,
-		nullptr,
-		nullptr,
-		nullptr);
+    hMainWindow = CreateWindow(wc.lpszClassName,
+                               L"My Title",
+                               WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                               100,
+                               100,
+                               800,
+                               900,
+                               nullptr,
+                               nullptr,
+                               nullptr,
+                               nullptr);
 
-	MSG msg{};
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+    MSG msg{};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-	return 0;
+    return 0;
 }
 
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
-	switch (msg)
-	{
-	case WM_CREATE:
-		break;
+    switch (msg)
+    {
+    case WM_CREATE:
+        AddControls(hwnd);
+        break;
 
-		break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
 
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
+    default:
+        return DefWindowProc(hwnd, msg, wp, lp);
+    }
+}
 
-	default:
-		return DefWindowProcW(hwnd, msg, wp, lp);
-	}
+void AddControls(HWND hwnd)
+{
+    CreateWindow(L"Button",
+                 L"Open File",
+                 WS_VISIBLE | WS_CHILD,
+                 10,
+                 10,
+                 150,
+                 36,
+                 hwnd,
+                 reinterpret_cast<HMENU>(OPEN_FILE_BUTTON),
+                 nullptr,
+                 nullptr);
+
+    CreateWindow(L"Button",
+                 L"Save File",
+                 WS_VISIBLE | WS_CHILD,
+                 170,
+                 10,
+                 150,
+                 36,
+                 hwnd,
+                 reinterpret_cast<HMENU>(SAVE_FILE_BUTTON),
+                 nullptr,
+                 nullptr);
+
+    hEdit =
+        CreateWindow(L"Edit",
+                     L"",
+                     WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_BORDER |
+                         ES_AUTOHSCROLL | ES_AUTOVSCROLL | WS_VSCROLL | WS_HSCROLL,
+                     10,
+                     50,
+                     400,
+                     300,
+                     hwnd,
+                     nullptr,
+                     nullptr,
+                     nullptr);
 }
