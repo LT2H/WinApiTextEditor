9a18280aa22c220f96cd8dfddd1d395c208e066f
LT2H <letranhuuhung@gmail.com>
2025-02-07 10:17:27 +0700

    feat: add file open and read functionality to 'Open File' button

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -1,9 +1,17 @@
 #include "Core/Core.h"
 #include <windows.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 LRESULT windowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 
 void addControls(HWND hwnd);
+
+int displayFile(const std::wstring& path);
+
+void openFile(HWND hwnd);
 
 constexpr int OPEN_FILE_BUTTON{ 1 };
 constexpr int SAVE_FILE_BUTTON{ 2 };
@@ -55,6 +63,20 @@ LRESULT windowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
         addControls(hwnd);
         break;
 
+    case WM_COMMAND:
+        switch (wp)
+        {
+        case OPEN_FILE_BUTTON:
+        {
+            openFile(hwnd);
+            break;
+        }
+        default:
+            break;
+        }
+
+        break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -103,4 +125,41 @@ void addControls(HWND hwnd)
                      nullptr,
                      nullptr,
                      nullptr);
+}
+
+int displayFile(const std::wstring& path)
+{
+    std::wifstream file(path, std::ios::binary);
+    if (!file)
+    {
+        MessageBox(nullptr, L"Failed to open file!", L"Error", MB_OK | MB_ICONERROR);
+        return -1;
+    }
+
+    std::wstringstream buffer{};
+    buffer << file.rdbuf();
+
+    std::wstring content{};
+    content = buffer.str();
+
+    SetWindowText(hEdit, content.data());
+}
+
+void openFile(HWND hwnd)
+{
+    OPENFILENAME ofn{};
+    std::vector<wchar_t> fileName(100); // Initialize with null characters
+
+    ofn.lStructSize  = sizeof(OPENFILENAME);
+    ofn.hwndOwner    = hwnd;
+    ofn.lpstrFile    = fileName.data();
+    ofn.nMaxFile     = static_cast<DWORD>(fileName.size());
+    ofn.lpstrFilter  = L"All Files\0*.*\0Source Files\0*.CPP\0Text Files\0*.TXT\0";
+    ofn.nFilterIndex = 1;
+
+    if (GetOpenFileName(&ofn))
+    {
+        MessageBox(nullptr, fileName.data(), L"Selected File", MB_OK);
+        displayFile(ofn.lpstrFile);
+    }
 }
