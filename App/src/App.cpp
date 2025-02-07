051588de7db74e0c2ebfc0b8d4ecc4c12b715266
LT2H <letranhuuhung@gmail.com>
2025-02-07 09:59:15 +0700

    feat: add initial main window implementation

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -1,6 +1,60 @@
 #include "Core/Core.h"
+#include <windows.h>
 
-int main()
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+HWND hMainWindow{};
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
-	Core::PrintHelloWorld();
+	// MessageBox(nullptr, L"HELLO", L"My title", MB_OK);
+	WNDCLASSW wc{};
+
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hInstance = hInst;
+	wc.lpszClassName = L"myWindowClass";
+	wc.lpfnWndProc = WindowProc;
+
+	if (!RegisterClassW(&wc))
+		return -1;
+
+	hMainWindow = CreateWindowW(wc.lpszClassName,
+		L"My Title",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		100,
+		100,
+		800,
+		900,
+		nullptr,
+		nullptr,
+		nullptr,
+		nullptr);
+
+	MSG msg{};
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		break;
+
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProcW(hwnd, msg, wp, lp);
+	}
 }
