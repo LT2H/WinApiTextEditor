19168ea1ef0c18c007aff0c2b1ab647a8db47ae4
LT2H <letranhuuhung@gmail.com>
2025-02-21 10:47:10 +0700

    feat: add "New Window" menu control

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -12,63 +12,13 @@
 #include <sstream>
 #include <vector>
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+void createNewWindow(HINSTANCE hInst)
 {
-    Core::enableDebugConsole();
-
     Core::Window mainWindow{ hInst,       IDC_ARROW, COLOR_WINDOW, L"myWindowClass",
                              L"My Title", 100,       100,          800,
                              900,         nullptr };
     mainWindow.registerHotkeys();
     auto mainWindowHwnd{ mainWindow.getHandle() };
-
-    /* Core::Control openFileButon{
-         L"Button", L"Open File",   WS_VISIBLE | WS_CHILD,  10, 10, 150,
-         36,        mainWindowHwnd, Core::Command::openFile
-     };
-
-     Core::Control saveFileButon{
-         L"Button", L"Save File",   WS_VISIBLE | WS_CHILD,  170, 10, 150,
-         36,        mainWindowHwnd, Core::Command::saveFile
-     };
-
-     Core::Control editField{ L"Edit",
-                              L"",
-                              WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_BORDER |
-                                  ES_AUTOHSCROLL | ES_AUTOVSCROLL | WS_VSCROLL |
-                                  WS_HSCROLL,
-                              10,
-                              50,
-                              400,
-                              300,
-                              mainWindowHwnd };
-
-     openFileButon.addHandler([mainWindowHwnd, editField]()
-                              { openFile(mainWindowHwnd, editField); });
-
-     saveFileButon.addHandler([mainWindowHwnd, editField]()
-                              { saveFile(mainWindowHwnd, editField); });*/
-
-    // auto fileMenu{ std::make_unique<Core::Menu>(
-    //     MF_POPUP, Core::Command::nothing, L"File") };
-    // auto newMenu{ std::make_unique<Core::Menu>(
-    //     MF_STRING, Core::Command::nothing, L"New") };
-
-    // auto openSubMenu{ std::make_unique<Core::Menu>(
-    //     MF_POPUP, Core::Command::nothing, L"Open SubMenu") };
-    // auto changeTitleSubMenu{ std::make_unique<Core::Menu>(
-    //     MF_STRING, Core::Command::nothing, L"Change Title") };
-
-    // openSubMenu->appendMenu(std::move(changeTitleSubMenu));
-
-    //// auto seperator{ std::make_unique<Core::Menu>(MF_SEPARATOR) };
-    // auto exitMenu{ std::make_unique<Core::Menu>(
-    //     MF_STRING, Core::Command::nothing, L"Exit") };
-
-    // fileMenu->appendMenu(std::move(newMenu));
-    // fileMenu->appendMenu(std::move(openSubMenu));
-    //// fileMenu->appendMenu(seperator);
-    // fileMenu->appendMenu(std::move(exitMenu));
 
     Core::Control editField{ L"Edit",
                              L"",
@@ -99,6 +49,78 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsho
     mainWindow.registerFunc(Core::Command::saveFileAs,
                             [mainWindowHwnd, editField]()
                             { saveFile(mainWindowHwnd, editField); });
+
+    fileMenu->appendMenu(
+        MF_STRING, Core::Command::newWindow, L"New Window\tCtrl+Shift+N");
+    mainWindow.registerFunc(
+        Core::Command::newWindow,
+        []()
+        { ShellExecute(NULL, L"open", L"App.exe", NULL, NULL, SW_SHOWNORMAL); });
+
+    fileMenu->appendMenu(MF_SEPARATOR);
+
+    fileMenu->appendMenu(MF_STRING, Core::Command::exit, L"Exit");
+
+    mainMenu->appendMenu(MF_POPUP, fileMenu->getHwndMenu(), L"File");
+
+    mainMenu->setMenu();
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+    Core::enableDebugConsole();
+
+    Core::Window mainWindow{ hInst,       IDC_ARROW, COLOR_WINDOW, L"myWindowClass",
+                             L"My Title", 100,       100,          800,
+                             900,         nullptr };
+    mainWindow.registerHotkeys();
+    auto mainWindowHwnd{ mainWindow.getHandle() };
+
+    Core::Control editField{ L"Edit",
+                             L"",
+                             WS_VISIBLE | WS_CHILD | ES_MULTILINE | WS_BORDER |
+                                 ES_AUTOHSCROLL | ES_AUTOVSCROLL | WS_VSCROLL |
+                                 WS_HSCROLL,
+                             10,
+                             50,
+                             400,
+                             300,
+                             mainWindowHwnd };
+
+    auto mainMenu{ std::make_unique<Core::Menu>(mainWindowHwnd) };
+
+    auto fileMenu{ std::make_unique<Core::Menu>() };
+
+    auto subMenu{ std::make_unique<Core::Menu>() };
+
+    auto helpMenu{ std::make_unique<Core::Menu>() };
+
+    fileMenu->appendMenu(MF_STRING, Core::Command::openFile, L"Open...\tCtrl+O");
+    mainWindow.registerFunc(Core::Command::openFile,
+                            [mainWindowHwnd, editField]()
+                            { openFile(mainWindowHwnd, editField); });
+
+    fileMenu->appendMenu(
+        MF_STRING, Core::Command::saveFileAs, L"Save As...\tCtrl+Shift+S");
+    mainWindow.registerFunc(Core::Command::saveFileAs,
+                            [mainWindowHwnd, editField]()
+                            { saveFile(mainWindowHwnd, editField); });
+
+    fileMenu->appendMenu(
+        MF_STRING, Core::Command::newWindow, L"New Window\tCtrl+Shift+N");
+    mainWindow.registerFunc(
+        Core::Command::newWindow,
+        []
+        {
+            ShellExecute(
+                NULL,
+                L"open",
+                L"D:\\Hung_Dev\\CPP\\repos\\WinApiTextEditor\\Binaries\\windows-x86_"
+                L"64\\Debug\\App\\App.exe",
+                NULL,
+                NULL,
+                SW_SHOWNORMAL);
+        });
 
     fileMenu->appendMenu(MF_SEPARATOR);
 
--- Core/src/Core/Utils/utils.h
+++ Core/src/Core/Utils/utils.h
@@ -8,11 +8,12 @@ constexpr UINT WM_CREATE_CONTROLS = WM_APP + 1;
 
 enum class Command
 {
-    nothing    = 1,
-    openFile   = 2,
-    saveFileAs = 3,
-    help       = 4,
-    exit       = 5,
+    nothing = 1,
+    openFile,
+    saveFileAs,
+    help,
+    exit,
+    newWindow,
 };
 
 // inline std::unordered_map<Command, std::string> shortcutKeys{
